48f59890623903275f21d9d4aa171a5f47ca7941
carlosruedasarmiento <cruedas@unal.edu.co>
2025-04-22 17:55:35 -0500

    Primera versión de tarea_1

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese su no: ");
+        System.out.print("Ingrese su nombre: ");
         String nombre = scanner.nextLine();
         System.out.print("Ingrese su edad: ");
         Integer edad = scanner.nextInt();
