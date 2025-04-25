059114d1ea08317c7f89071e3731a5f5984a236a
carlosruedasarmiento <cruedas@unal.edu.co>
2025-04-24 19:29:53 -0500

    cambio edad

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -19,13 +19,28 @@ public class JavaApplication1 {
         System.out.print("Ingrese su nombre: ");
         String nombre = scanner.nextLine();
         System.out.print("Ingrese su edad: ");
-        Integer edad = scanner.nextInt();
-        String edadString = edad.toString();
+        int edad = scanner.nextInt();
         System.out.print("Ingrese su numero de telefono: ");
         String telefono = scanner.next();
+        scanner.nextLine();
         System.out.print("Ingrese su carrera: ");
         String carrera = scanner.nextLine();
-        System.out.print(nombre + " " + edadString + " " + telefono + " " + carrera);
+        //pasa la variable nombre a ascii sin usar bucles y guardas los varoles en un arreglo
+        int[] ascii = new int[nombre.length()];
+        for (int i = 0; i < nombre.length(); i++) {
+            ascii[i] = (int) nombre.charAt(i);
+        }
+        //imprime el arreglo ascii
+        System.out.print("El nombre en ascii es: ");
+        for (int i = 0; i < ascii.length; i++) {
+            System.out.print(ascii[i]);
+            if (i != ascii.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("Su edad es: " + edad);
+        System.out.println("Su numero de telefono es: " + telefono);
+        System.out.println("Su carrera es: " + carrera);
     }
     
 }
